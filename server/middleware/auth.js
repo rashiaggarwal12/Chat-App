f109c244dcaa9ab6001c33ac04e22deb7c9c2f5b
rashiaggarwal12 <rashiaggarwal0510@gmail.com>
2025-07-12 00:04:37 +0530

    first commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+//middleware is the function that is executed before executing the controller function. using middleware function we can protect our route so if the user is authenticated then only they can access that particular api endpoint.
+
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+//Middleware to protect routes
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.headers.token;
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) return res.json({ success: false, message: "User not found" });
+        
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
